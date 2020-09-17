40fef95a7a609a51dc4422e2d1298b456a20c45f
kushal-7 <71422133+kushal-7@users.noreply.github.com>
2020-09-17 10:02:49 +0600

    Add files via upload

---
+++ array_demo.java
@@ -0,0 +1,15 @@
+public class array_demo{
+
+	public static void main(String [] args)
+	{
+		
+		int [] array ={1,7,3,8};
+		for(int i=0; i<array.length ;i++)
+		{
+			System.out.print(array[i]);
+		}
+	
+	}
+
+
+}
